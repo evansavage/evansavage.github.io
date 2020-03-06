fc14b8c33627a9d984ee4334f228a303d1ede010
evansavage <esav678@gmail.com>
2020-03-06 15:52:45 -0500

    More music control updates HOPEFULLY RIGHT THIS TIME

--- assets/js/music-player.js
+++ assets/js/music-player.js
@@ -33,7 +33,7 @@ function pageFullyLoaded(e) {
 
 			widget = SC.Widget(el);
 			window._total_duration = 0;
-      var toggleVar = 1;
+      var toggleVar = 0;
       var seekVar = 0;
 			widget.bind(SC.Widget.Events.READY, function(){
 				widget.getDuration(function(duration){
@@ -84,17 +84,26 @@ function pageFullyLoaded(e) {
       // document.getElementById('toggle-play').className = "toggle-play play";
       // widget.skip('2');
       parentContainer.find('.toggle-play').on('click', function(){
-        if (toggleVar % 2 == 0) {
+
+				if (toggleVar % 2 == 0) {
           widget.play();
           parentContainer.find('.pause-svg').css('display', 'block');
 					parentContainer.find('.play-svg').css('display', 'none');
+					if (screenWidth <= 600) {
+						$('.bottom-player-wrapper .music-control-wrapper .toggle-play').find('.pause-svg').css('display', 'block');
+						$('.bottom-player-wrapper .music-control-wrapper .toggle-play').find('.play-svg').css('display', 'none');
+					}
         }
         else if (toggleVar % 2 == 1) {
           widget.pause();
 					parentContainer.find('.pause-svg').css('display', 'none');
 					parentContainer.find('.play-svg').css('display', 'block');
+					if (screenWidth <= 600) {
+						$('.bottom-player-wrapper .music-control-wrapper .toggle-play').find('.pause-svg').css('display', 'none');
+						$('.bottom-player-wrapper .music-control-wrapper .toggle-play').find('.play-svg').css('display', 'block');
+					}
         }
-        toggleVar += 1;
+				toggleVar += 1;
         toggleVar = toggleVar % 2;
       });
 
@@ -160,14 +169,14 @@ function pageFullyLoaded(e) {
 
       listener.simple_combo("space", function(e) {
         e.preventDefault();
-        if (toggleVar % 2 == 1) {
+        if (toggleVar % 2 == 0) {
           widget.play();
 					parentContainer.find('.pause-svg').css('display', 'block');
 					parentContainer.find('.play-svg').css('display', 'none');
 					$('.bottom-player-wrapper .music-control-wrapper').find('.pause-svg').css('display', 'block');
 					$('.bottom-player-wrapper .music-control-wrapper').find('.play-svg').css('display', 'none');
         }
-        else if (toggleVar % 2 == 0) {
+        else if (toggleVar % 2 == 1) {
           widget.pause();
 					parentContainer.find('.pause-svg').css('display', 'none');
 					parentContainer.find('.play-svg').css('display', 'block');
@@ -255,18 +264,22 @@ function pageFullyLoaded(e) {
 
 		  });
 			$('.bottom-player-wrapper .music-control-wrapper .toggle-play').on('click', function() {
+				toggleVar += 1;
+				toggleVar = toggleVar % 2;
 				if (toggleVar % 2 == 1) {
 					widget.play();
 					$(this).find('.pause-svg').css('display', 'block');
 					$(this).find('.play-svg').css('display', 'none');
+					$('#myNav .tm .music-control-wrapper').find('.pause-svg').css('display', 'block');
+					$('#myNav .tm .music-control-wrapper').find('.play-svg').css('display', 'none');
 				}
 				else if (toggleVar % 2 == 0) {
 					widget.pause();
 					$(this).find('.pause-svg').css('display', 'none');
 					$(this).find('.play-svg').css('display', 'block');
+					$('#myNav .tm .music-control-wrapper').find('.pause-svg').css('display', 'none');
+					$('#myNav .tm .music-control-wrapper').find('.play-svg').css('display', 'block');
 				}
-				toggleVar += 1;
-				toggleVar = toggleVar % 2;
 			});
       // $(window).keyup(function(e){
       //
