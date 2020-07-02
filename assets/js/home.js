2d3b196091ef355c2bb0fe908b19affff366e116
evansavage <esav678@gmail.com>
2020-07-02 12:26:51 -0400

    Added random header glitch effect

--- assets/js/home.js
+++ assets/js/home.js
@@ -14,9 +14,60 @@ function glitchload() {
   // console.log(homeWraps);
   // toggle = 1
 }
+function glitchRand() {
+  var titles = ['.music-title', '.producer-title', '.research-title', '.web-title']
+  // while ($('.about-content-container').css('display') == 'block') {
+  //   var length = 2000
+  //   console.log('yeet');
+  //   var burst = Math.floor(Math.random() * 10)
+  //   var selection = titles[Math.floor(Math.random() * titles.length)];
+  //   // var length = Math.floor(Math.random() * 2000)
+  //   setTimeout(
+  //     $(selection).addClass('glitch')
+  //   , Math.floor(length / burst));
+  //   $(selection).removeClass('glitch');
+  // }
 
+
+}
+// document.fonts.ready.then(function() {
+//   var aboutHeaderText = ['MUSIC TECHNOLOGIST', 'PRODUCER', 'AI RESEARCHER', 'DEVELOPER']
+//   var aboutSelectors = ['.music-title', '.producer-title', '.research-title', '.web-title']
+//   $.each(aboutHeaderText, function(i, value) {
+//     var text = new Blotter.Text(value, {
+//       family : "microgramma, serif",
+//       size : 27,
+//       fill : "#36413E"
+//     })
+//     var material = new Blotter.LiquidDistortMaterial();
+//
+//     // Play with the value for uSpeed. Lower values slow
+//     // down animation, while higher values speed it up. At
+//     // a speed of 0.0, animation is stopped entirely.
+//     material.uniforms.uSpeed.value = 0.25;
+//     var blotter = new Blotter(material, {
+//       texts : text
+//     });
+//     var elem = $(aboutSelectors[i]);
+//     var scope = blotter.forText(text);
+//     scope.appendTo(elem);
+//   });
+// }.bind(this));
 $(document).ready(function(){
-  console.log('yes');
+  // console.log('yes');
+  // glitchRand();
+  var titles = ['.music-title', '.producer-title', '.research-title', '.web-title']
+  if ($('.about-content-container').css('display') == 'block') {
+
+  }
+  window.setInterval(function(){
+    /// call your function here
+    var selection = titles[Math.floor(Math.random() * titles.length)];
+    // setTimeout(
+    $(selection).toggleClass('glitch')
+    // , Math.floor(Math.random() * 1500));
+    // $(selection).removeClass('glitch')
+  }, Math.floor(Math.random() * 300));
   if (window.outerWidth > 769) {
     // $('.home-wrap').css('display', 'none');
     // $('.music-title').hover(function () {
@@ -32,6 +83,8 @@ $(document).ready(function(){
     //   $('.home-wrap.w').fadeToggle('fast');
     // });
   }
+
+
 
   // $('.home-wrap').fadeIn('fast');
   // while (toggle != 8) {
